8ab648f889305631f22fefbe7ee91a733a58ae58
seoryeongwi <112676971+seoryeongwi@users.noreply.github.com>
2022-10-06 14:57:20 +0900

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,29 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("환영합니다.")
+환영합니다.
+>>> print("파이썬의 세계에 오신 것을 환영합니다.")
+파이썬의 세계에 오신 것을 환영합니다.
+>>> print("파이썬은 강력합니다.")
+파이썬은 강력합니다.
+>>> print("반갑습니다. 파이썬!")
+반갑습니다. 파이썬!
+>>> print(2*3/10)
+0.6
+>>> print("Hello","World", "!!!")
+Hello World !!!
+>>> print(7*24)
+168
+>>> import turtle
+>>> t = turtle.Turtle()
+>>> t.shape("turtle")
+>>> t.forward(100)
+>>> t.left(90)
+>>> t.forward(100)
+>>> t.right(90)
+>>> t.forward(100)
+>>> t.right(90)
+>>> t.forward(100)
+>>> t.left(90)
+>>> t.forward(100)
+>>> 
